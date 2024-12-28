565bd480be2a7798f240aa32e398481dd03e1093
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2024-12-28 14:43:52 +0530

    signup and login page done

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
-import "dotenv/config";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./database/connectDB.js";
 import authRoutes from "./routes/auth.routes.js";
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
